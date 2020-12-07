16b5c644fac7987cd4a12c7e8130b0490e48d6e1
Demetris Gerogiannis <dgerog@gmail.com>
2020-12-08 01:10:43 +0200

    Fixed bugs. Added testing pipeline.

--- theStudy.py
+++ theStudy.py
@@ -17,8 +17,18 @@ class theStudy:
             readTable: Get the columns of an excel/csv file
             kneeThresholding: Knee thresholding on the covariance matrix - Estimate number of clusters
             prepareCrossValidation: Split the training set in two subsets (random sampling) for cross validation
+            classificationAnalysis: Analyse the clasification results (Precission/Recall - F1 Measure)
     """
-
+    
+    def __init__(self):
+        #initialize properties
+        self.X = []
+        self.F = []
+        self.isPatient = []
+        self.isActive = []
+        self.isNonActive = []
+        self.patientRecs = []
+        self.healthyRecs = []
     #
     # Public
     #
@@ -43,7 +53,7 @@ class theStudy:
             _kernelCov: A string with the preprocessing on the data. None for no preprocessing.
             _oFName: The full path to save the plots. None for no ploting
                            
-            RETURN: (x_max, y_max): Index and value of the threashold.
+            RETURN: Number of clusters (estimated)
         """
         #
         # Part 1. Estimate the threshold
@@ -55,7 +65,7 @@ class theStudy:
         
         # compute covariance matrix
         if (not _oFName is None):
-            #kernel PCA
+            #kernel PCA - To DO
             S = np.cov(_X)
         else:
             #linear (typical cov matrix)
@@ -96,6 +106,7 @@ class theStudy:
             plt.plot([P0[0], P1[0]], [P0[1], P1[1]], 'gh-', linewidth=3, markersize=6)
             plt.plot([P0[0], P2[0]], [P0[1], P2[1]], 'gh-', linewidth=3, markersize=6)            
             plt.plot(P0[0], P0[1], 'ks', linewidth=3, markersize=6)
+            plt.title('K = ' + str(P0[0]))
             plt.savefig(_oFName, bbox_inches='tight', pad_inches=1)
             plt.close()
         
@@ -104,7 +115,7 @@ class theStudy:
     """
         Method: readTable
     """ 
-    def readTable(self, _path,  _colsToRead, _sheetToRead=0):
+    def readTable(self, _path,  _colsToRead, _sheetToRead=0, _doAppend=False, _doFilterData=False):
         """
             Read an excel/csv file to extract the table data.
             Patients' records are stored vertically (Columns)
@@ -113,12 +124,18 @@ class theStudy:
             _colsToRead (str): Columns to read, eg. D:AF meaning read cols from D to AF. None to read all.
                                IMPORTANT: if _headersRow is True then make sure the headers row is also include.
             _sheetToRead (int): Which sheet to read (1 for the first sheet, etc.)
+            _doAppend: Instruct if the new records will be added to the end of the existing records.
+                        FALSE: Initialize with the data - Drop old records (default).
+                        TRUE: Append to existing records.
+            _doFilterData: Wheather to remove max value (True/False)
         """
         # Read data
-        T = pd.read_excel(io=_path, header=None, usecols=_colsToRead, sheet_namestr=_sheetToRead, dtype=str).values
+        T = pd.read_excel(io=_path, header=None, usecols=_colsToRead, sheet_name=_sheetToRead, dtype=str).values
 
         #
         # Get extra features
+        #
+        # _Lines start from index 0_
         #
         # Line 1: STATUS
         # Line 2: PCR
@@ -126,25 +143,39 @@ class theStudy:
         # Line 4: RAPID TEST
         # Line 5: SMOKING
         # Line 6: AGE (YEARS)
-        self.F = T[0:6, :]
+        F = T[0:6, :]
 
         #
         # Read the respiratory data
         #
-        self.X = T[7:,:].astype(float)
+        X = T[7:,:].astype(float)
+
+        if _doFilterData:
+            # upper bound filter
+            for i in range(0,X.shape[1]):
+                th = np.max(X[:,i])
+                X[X[:,i]>=th,i] = np.min(X[:,i])
 
         #
         # Assign patient labels
         #
-        self.isPatient = (self.F[0,:] == 'P').astype(bool)
-        self.infectedLabel = (self.F[2,:].astype(int)*self.F[3,:].astype(int)).astype(bool)
+        isPatient = (F[0,:] == 'P').astype(bool)
 
-        #
-        # Detect the patients' groups (Indices of Postive/Negative)
-        #
-        self.patientRecs = np.where(self.isPatient == True)
-        self.healthyRecs = np.where(self.isPatient == False)
+        #isActive = (isPatient == True) and (F[2,:].astype(bool) or F[3,:].astype(bool))
+        #isNonActive = (isPatient == True) and not isActive
 
+        if (_doAppend):
+            self.X = np.concatenate((self.X, X), axis=1) if len(self.X) else X
+            self.F = np.concatenate((self.F, F), axis=1) if len(self.F) else F
+            self.isPatient = np.concatenate((self.isPatient, isPatient)) if len(self.isPatient) else isPatient
+            #self.isActive = np.concatenate((self.isActive, isActive)) if len(self.isActive) else isActive
+            #self.isNonActive = np.concatenate((self.isNonActive, isNonActive)) if len(self.isNonActive) else isNonActive
+        else:
+            self.X = X
+            self.F = F
+            self.isPatient = isPatient
+            #self.isActive = isActive
+            #self.isNonActive = isNonActive
 
     """
         Method: prepareCrossValidation
@@ -155,6 +186,8 @@ class theStudy:
             
             _trainPct: (% in (0,1)) - percentage of data to use for the training set
                         Default is 10-fold cross validation
+
+            RETURN: (trainInd, testInd): The indices of self.X to contain the training and testing observations respectively.
         """
 
         if _trainPct <=0 or _trainPct >=1:
@@ -172,7 +205,7 @@ class theStudy:
     """
         Method: classificationAnalysis
     """ 
-    def classificationAnalysis(self, _Y):
+    def classificationAnalysis(self, _Y, _Ind):
         """
             Analyze the classification results.
             Compute:
@@ -181,6 +214,9 @@ class theStudy:
                 F1 Measure
             
             _Y: The computed labels - Analyze this classification result (True/False).
+            _Ind: The index of the observation => Y[i] is tested against self.X[_Ind[i]]
+
+            RETURN: (P,R,F1): Precission, Recall, F1 Measure
         """
 
         TP = 0 # True Positive
@@ -189,17 +225,17 @@ class theStudy:
         FN = 0 # False Negative
 
         for i in range(0,_Y.shape[0]):
-            if self.isPatient[i] and _Y[i]:
+            if self.isPatient[_Ind[i]] and _Y[i]:
                 TP = TP + 1
-            elif self.isPatient[i] and not _Y[i]:
+            elif self.isPatient[_Ind[i]] and not _Y[i]:
                 FP = FP + 1
-            elif not self.isPatient[i] and _Y[i]:
+            elif not self.isPatient[_Ind[i]] and _Y[i]:
                 FN = FN + 1
-            elif not self.isPatient[i] and not _Y[i]:
+            elif not self.isPatient[_Ind[i]] and not _Y[i]:
                 TN + TN + 1
 
         R = TP/(TP+FN)
         P = TP/(TP+FP)
-        F1 = 2*P*R/(P+R)
+        F1 = 2*P*R/((P+R) + .0000000000000000001)
 
         return (P,R,F1)
